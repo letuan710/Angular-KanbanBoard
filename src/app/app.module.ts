2b6a1eaae07b410e6ee61a9a01bcc8f1a40b9816
Tuấn <tuanlp@bidv.com.vn>
2020-11-05 17:31:02 +0700

    a

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
+import {MatInputModule} from '@angular/material/input';
+import {MatListModule} from '@angular/material/list';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+import { Column1Component } from './column1/column1.component';
+import { Column2Component } from './column2/column2.component';
+import { Column3Component } from './column3/column3.component';
+import { Column4Component } from './column4/column4.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    Column1Component,
+    Column2Component,
+    Column3Component,
+    Column4Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatCardModule,
+    MatInputModule,
+    MatListModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
